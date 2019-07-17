58c61d136c2bd9d40869e4a7d3df58662bb3fc36
Tho Huynh <tho.hd@workhouse.me>
2019-07-17 12:29:20 +0700

    ADD .gitinore

--- resources/assets/js/Calendar.js
+++ resources/assets/js/Calendar.js
@@ -279,7 +279,7 @@ Calendar.prototype = {
       self.loadBookInCalendar(
         {
           header: {
-            left: `prev,next today`,
+            left: `prev,next     today`,
             right: `title`,
             center: `agendaWeek,agendaDay,listWeek`
           },
@@ -333,45 +333,10 @@ Calendar.prototype = {
                 dateEndCreate: start.format(`YYYY-MM-DD`)
               }
 
-
-              require('./bootstrap');
-
-
-                // Khởi tạo một đối tượng Pusher với app_key
-                var pusher = new Pusher('ea4c1db20cea941f0651', {
-                  cluster: 'ap1',
-                  encrypted: true
-                });
-
-                //Đăng ký với kênh chanel-demo-real-time mà ta đã tạo trong file DemoPusherEvent.php
-                var channel = pusher.subscribe('create-booking');
-
-                //Bind một function addMesagePusher với sự kiện DemoPusherEvent
-                channel.bind('App\\Events\\CreateBooking', addMessageDemo);
-
-                function addMessageDemo (data) {
-                  let book = data
-                  let Booking = new Book
-                  let alertSweet = new Sweet
-                  // let eventChang = Booking.createViewBoard(book.booking.id, book.booking.room_id, book.booking.title, book.booking.description, `2018-07-30T07:00:00`, `2018-07-30T08:00:00`)
-                  let alertAdmin = alertSweet.alertError()
-
-                  let eventData = {
-                    id : book.booking.book['id'],
-                    resourceId: book.booking.book['room_id'],
-                    title: book.booking.book['description'],
-                    description: `${book.booking.book['id']}-${book.booking.book['description']}`,
-                    start: `${book.booking.book['day']}T${book.booking.book['hour_start']}`,
-                    end: `${book.booking.book['day']}T${book.booking.book['hour_end']}`
-                  };
-                  console.log(eventData)
-                }
-
-
-
               self.fillDataToModalCreate(paramfillDataToModal)
-              self.showModalBootstrapCreate()
-              self.element.calendar.fullCalendar(`unselect`)
+              $('#modalCreateBooking').modal('show')
+              //self.showModalBootstrapCreate()
+              //self.element.calendar.fullCalendar(`unselect`)
             }
           },
           eventResize: function(event, delta, revertFunc) {
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <!-- CSS -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <script src=" https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ mix('scss/app.css') }}">
     <link rel="stylesheet" href="{{ mix('css/all.css') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -30,15 +31,10 @@
 <body id="bodyBroomServer">
 @yield('content')
 <!-- Javascript -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="{{ mix('/js/all.js') }}"></script>
     <script src="{{ mix('/js/home.js')}}"></script>
-<script src="http://broom.com:3000/socket.io/socket.io.js"></script>
-<script>
-  var socket = io.connect('http://broom.com:3000');
-  socket.on('news', function (data) {
-    console.log(data);
-    socket.emit('my other event', { my: 'data' });
-  });
-</script>
 </body>
 </html>
